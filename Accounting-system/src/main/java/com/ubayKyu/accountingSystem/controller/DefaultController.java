2ac7db1164ac983d06f8a3d6cdd655dd84ba7455
who000238 <who000238@gmail.com>
2021-12-10 15:18:05 +0800

    推送 ------------- 2021/12/10
    -------------
    工作日程表12/6工作日結束
    預設頁功能完成、連接資料庫且輸出資料至頁面上成功

--- Accounting-system/src/main/java/com/ubayKyu/accountingSystem/controller/DefaultController.java
+++ Accounting-system/src/main/java/com/ubayKyu/accountingSystem/controller/DefaultController.java
@@ -1,13 +1,40 @@
 package com.ubayKyu.accountingSystem.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ubayKyu.accountingSystem.entity.DefaultPageModel;
+import com.ubayKyu.accountingSystem.repository.DefaultRepository;
 
 @Controller
 public class DefaultController {
+
+	@Autowired
+	DefaultRepository defaultRepository;
 	
 	@GetMapping("/Default")
-	public String Default() {
+	public String Default(Model model) {
+//		List<DefaultPageModel> list = defaultRepository.getFindAll();
+		model.addAttribute("firstRecord", defaultRepository.firstRecord() );
+		model.addAttribute("lastRecord", defaultRepository.lastRecord());
+		model.addAttribute("recordCount", defaultRepository.recordCount());
+		model.addAttribute("memberCount", defaultRepository.memberCount());
 		return "Default";
 	}
+
+//	@Autowired
+//	DefaultPageRepository defaultPageRepository;
+//
+//	@GetMapping("/Default")
+//	public String Default(Model model) {
+//		model.addAttribute("firstRecord", defaultPageRepository.newest());
+//		model.addAttribute("lastRecord", defaultPageRepository.latest());
+//		model.addAttribute("recordCount", "123");
+//		model.addAttribute("memberCount", "123");
+//		return "Default";
+//	}
 }
